0cca8cd6cc89444996096ddc3f2a4465c446248f
Emile Mouannes <emile.mouannes@outlook.com>
2022-09-29 22:01:11 +0000

    Create project structure and npn setup

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8085,
+    hot: true
+  },
+};
