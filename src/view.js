3d911155920800e371481ed4ecefdf19cc3ae229
Emile Mouannes <emile.mouannes@outlook.com>
2022-11-03 17:00:31 -0400

    Add place ship highlight

--- src/view.js
+++ src/view.js
@@ -1,30 +1,53 @@
-let grid = document.querySelectorAll(".grid-left > .grid-layout > .cell-p1");
-
 let message = function (message) {
   let messageDOM = document.querySelector(".message > p");
   messageDOM.innerHTML = message;
 };
 
 let placeShip = function (shipSize) {
-  let direction = "y";
+  let grid = document.querySelectorAll(".grid-left > .grid-layout > .cell-p1");
+  let direction = "x";
+
+  // event listner function
+  let highlight = function (e, direction, shipSize) {
+    grid.forEach((elem) => elem.classList.remove("grid-hover-forced"))
+    let x = parseInt(e.target.id[12]);
+    let y = parseInt(e.target.id[6]);
+    if ((direction == "x" && x + shipSize > 10) || (direction == "y" && y + shipSize > 10)) {
+      throw new Error("Out of bounds");
+    } 
+
+    // convert from x y to linear coor
+    let coor = x * 1 + y * 10;
+    
+    if (direction == "x") {
+      let limit = coor + shipSize;
+      do {
+        grid[coor].classList.add("grid-hover-forced")
+        coor++;
+      } while (coor < limit);
+    } else if (direction == "y") {
+      let limit = coor + 10*shipSize;
+      do {
+        grid[coor].classList.add("grid-hover-forced")
+        coor += 10;
+      } while (coor < limit);
+    }
+  };
+
+  // event listener function
+  let rotateShip = function () {
+    direction = direction == "x" ? "y" : "x";
+  };
+  let gridListen = function() {
   grid.forEach((elem) => {
     elem.classList.add("grid-hover");
     elem.shipSize = shipSize;
-    elem.addEventListener("mouseover", highlight);
-  });
+    elem.addEventListener("mouseover", (e) => {
+      highlight(e, direction, shipSize);
+    });
+  })};
+  gridListen()
   window.addEventListener("wheel", rotateShip)
 };
-
-function highlight(e) {
-  let x = e.target.id[6];
-  let y = e.target.id[12];
-  console.log(`x:${x} & y:${y} ${e.target.shipSize} `);
-  console.dir(e.target);
-  console.log(grid);
-}
-
-function rotateShip(){
-  console.log("rotateShip")
-}
 
 module.exports = { placeShip };
