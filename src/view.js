fa484441916175e1968d212a6eddc476f7235f1c
Emile Mouannes <emile.mouannes@outlook.com>
2022-11-04 19:50:09 -0400

    Add highlight interference in red

--- src/view.js
+++ src/view.js
@@ -45,7 +45,7 @@ let displayGrid = (function () {
             console.log(error.message);
           }
           message("Ship placed! The next one");
-          refresh("populate")
+          refresh("populate");
           refresh(mode, shipSize);
         });
       });
@@ -60,21 +60,24 @@ let displayGrid = (function () {
   let refresh = function (mode, shipSize) {
     // Clear the grid from all the current hightlights
     grid.forEach((elem) =>
-      elem.classList.remove("cell-hover-forced", "cell-hover-outBound")
+      elem.classList.remove(
+        "cell-hover-forced",
+        "cell-hover-outBound",
+        "cell-ship-highlight-interference"
+      )
     );
     // Populate the grid from memory
     if (mode == "populate") {
       let humanGrid = game.human.gameboard.getPrivateGrid();
       humanGrid.forEach((elemRow, x) => {
         elemRow.forEach((elemCell, y) => {
-          if (elemCell[0] == "S"){
-            console.log(`There is a ship at x:${x} and y: ${y}`)
+          if (elemCell[0] == "S") {
+            console.log(`There is a ship at x:${x} and y: ${y}`);
             let coorLinear = parseInt(x) + parseInt(y) * 10;
-            grid[coorLinear].classList.add("cell-ship-present")
+            grid[coorLinear].classList.add("cell-ship-present");
           }
-        })
-        }
-      );
+        });
+      });
     } else if (mode == "placeShip") {
       // convert from x y to linear coor
       let coorLinear = parseInt(mouseCoor.x) + parseInt(mouseCoor.y) * 10;
@@ -88,7 +91,24 @@ let displayGrid = (function () {
         grid[coorLinear].classList.add("cell-hover-outBound");
         return;
       }
-      // Highlight the grid based on coor and ship size
+      for (let i = 0; i < shipSize; i++) {
+        if (
+          mouseCoor.dir == "x" &&
+          grid[coorLinear + i].classList.contains("cell-ship-present")
+        ) {
+          grid[coorLinear + i].classList.add(
+            "cell-ship-highlight-interference"
+          );
+        } else if (
+          mouseCoor.dir == "y" &&
+          grid[coorLinear + i*10].classList.contains("cell-ship-present")
+        ) {
+          grid[coorLinear + i*10].classList.add(
+            "cell-ship-highlight-interference"
+          );
+        }
+      }
+      // Draw the ship highlight in x or y
       if (mouseCoor.dir == "x") {
         let limit = coorLinear + shipSize;
         do {
